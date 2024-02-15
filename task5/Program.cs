7ac96c5ccefe9f148469bdec517144d420d2c164
Koryaga18 <paxwild.ns@gmail.com>
2024-02-15 13:07:49 +0600

    Add project files.

---
+++ task5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ФИО");
+
+            string FullName = Console.ReadLine();
+
+            Console.WriteLine("Приветствую тебя - " + FullName);
+
+            Console.ReadLine();
+        }
+    }
+}
